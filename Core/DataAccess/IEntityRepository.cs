d3e1b26ece06ec8debe72b4bb7a1361eae88edd5
Serman KALEOGLU <sermankaleoglu03@gmail.com>
2021-03-11 22:42:41 +0300

    11.03.2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,19 +1,19 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint
     //class:referans tip olabilir.
     //IEntity:IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
     //new():new'lenebilir olmalı.
     //IEntity newlenemez,interfacedir.
-    public interface IEntityRepository<T>where T:class,IEntity,new()
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        List<T> GelAll(Expression<Func<T,bool>>filter=null);
+        List<T> GelAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
