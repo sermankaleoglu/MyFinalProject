46438caf473a6f5df9c23068ddedd8519ac30ea1
Serman KALEOGLU <sermankaleoglu03@gmail.com>
2021-03-18 00:52:59 +0300

    18.03.2021

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -16,6 +16,8 @@ namespace WebAPI.Controllers
     public class ProductsController : ControllerBase
     {
         //Loosely coupled
+        //naming convention
+        //IoC Container:Inversion of control
         IProductService _productService;
 
         public ProductsController(IProductService productService)
@@ -23,15 +25,39 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             //Dependency chain (bağımlılık zinciri)
             
-            var result = productService.GetAll();
-            return result.Data;
-
-            
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);            
         }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+        
     }
 }
