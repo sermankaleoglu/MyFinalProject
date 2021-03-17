fbbd8e6182148caaa1bc8a44bcef43fa645073b9
Serman KALEOGLU <sermankaleoglu03@gmail.com>
2021-03-17 22:30:47 +0300

    17.03.2021

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //newlememek için static yaptık.Sabit olmuş oldu.
+    public static class Messages
+    {
+        public static string ProductAdded = "ürün eklendi";
+        public static string ProductNameInvalid = "ürün ismi geçersiz";
+        public static string MaintenanceTime="Sistem bakımda";
+        public static string ProductsListed="Ürünler listelendi ";
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]//attribute(C#),annotation(Java)
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain (bağımlılık zinciri)
+            
+            var result = productService.GetAll();
+            return result.Data;
+
+            
+        }
+    }
+}
